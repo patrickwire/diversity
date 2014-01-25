f8e26bab8210adc85f31b8a4340a24bd4b8dc3c6
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2014-01-25 17:52:30 +0100

    use waitingpool, LOG instead of log

--- server/main.rb
+++ server/main.rb
@@ -2,6 +2,9 @@
 require 'eventmachine'
 require 'em-websocket'
 require 'json'
+require 'logger'
+
+LOG = Logger.new STDOUT
 
 class Player
   attr_reader :socket, :id
@@ -37,35 +40,88 @@ class Game
   end
 end
 
-waiting_players_by_socket_id = {}
+class PlayerPool
+  def initialize
+    @players_by_socket_id = {}
+  end
+
+  def <<(player)
+    send_players_waiting_update(@players_by_socket_id.length + 1)
+    @players_by_socket_id[player.socket.object_id] = player
+  end
+
+  def has_socket_id(sockid)
+    @players_by_socket_id.has_key? sockid
+  end
+
+  def playercount
+    @players_by_socket_id.length
+  end
+
+  def players
+    @players_by_socket_id.values
+  end
+
+  def clear
+    @players_by_socket_id.clear
+  end
+
+  def delete_by_socket_id(sockid)
+    @players_by_socket_id.delete(sockid)
+    send_players_waiting_update(@players_by_socket_id.length)
+  end
+
+private
+  def send_players_waiting_update(playercount)
+    @players_by_socket_id.each_value do |p|
+      LOG.debug("sending playerswaitingupdate to #{p.id}")
+      p.socket.send JSON.generate({
+        type: "PlayersWaitingUpdate",
+        currentPlayersWaiting: playercount
+      })
+    end
+  end
+end
+
+waitingpool = PlayerPool.new
 
 game = nil
+LOG.debug "FSK Server starting up..."
 
 EM.run do
   EM::WebSocket.start(host: "0.0.0.0", port: 8080) do |socket|
 
     socket.onopen do
+      LOG.debug("Client #{socket.object_id} connected")
+
       if game
         socket.send JSON.generate({type: "GameAlreadyRunning"})
         socket.close
-      elsif waiting_players_by_socket_id[socket.object_id]
+      elsif waitingpool.has_socket_id(socket.object_id)
         socket.send JSON.generate({type: "AlreadyRegistered"})
       else
         player = Player.new(socket)
-        waiting_players_by_socket_id[socket.object_id] = player
-        socket.send JSON.generate({type: "WaitingForPlayers", id: player.id})
+        waitingpool << player
+        socket.send JSON.generate({
+          type: "RegistrationSuccessful",
+          id: player.id,
+          currentPlayersWaiting: waitingpool.playercount
+        })
+        LOG.debug("Assigning id #{player.id}")
       end
     end
 
     socket.onmessage do |message_json|
+      LOG.debug("Got message: \"#{message_json}\"")
       message = JSON.parse(message_json)
-      if message.type == "StartGame"
+      if message["type"] == "StartGame"
         if game
           socket.send({type: "GameAlreadyRunning"})
-        elsif waiting_players_by_socket_id.length < 2
+        elsif waitingpool.playercount < 2
           socket.send({type: "NotEnoughPlayers"})
         else
-          game = Game.new(waiting_players_by_socket_id.values)
+          game = Game.new(waitingpool.values)
+          waitingpool.clear
         end
       elsif not game
         socket.send({type: "GameNotRunning"})
@@ -78,7 +134,8 @@ EM.run do
       game.abort if game and game.contains_player_with_socket_id(socket.object_id)
       game = nil
 
-      waiting_players_by_socket_id.delete(socket.object_id)
+      waitingpool.delete_by_socket_id(socket.object_id)
     end
   end
 end
+
